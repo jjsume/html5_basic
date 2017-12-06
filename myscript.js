4d426cf5ad5ab70906f79ca5735dc2fb42539a7e
jjsume <34176980+jjsume@users.noreply.github.com>
2017-12-06 20:59:47 +0200

    Updated 06_12_2017
    Added some interactive content. (Printing greeting text inside the paragraph instead of annoying window alert screen...)

--- myscript.js
+++ myscript.js
@@ -1,6 +1,13 @@
 function run(){
-    var name = document.getElementById("name").value; 
-    var heyYou = "Hello, " + name + "! \n" +"How are you today, my friend?";
-	window.alert(heyYou);
+	var name = document.getElementById("name").value; 
+	//Check if name is given...	
+	if (name == "") {
+		document.getElementById("results").innerHTML = "Give me your name,please!";		
+
+	}
+	else {
+		var msg = "Hello, "+name+"! How are you today, my friend?";
+		document.getElementById("results").innerHTML = msg
+	}
     return false; //To prevent it from going into next page.
 }
